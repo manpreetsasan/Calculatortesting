ce109801599c00d189b6f281fdb73c9f9aeea65c
manpreetsasan <50706112+manpreetsasan@users.noreply.github.com>
2019-05-23 10:46:22 -0400

    Add files via upload
    changed values again

--- Class1.cs
+++ Class1.cs
@@ -15,11 +15,11 @@ namespace Calculator
     class CalcTests
     {
         [Test]
-        public void GetAddition_Input20and20_Returns40()
+        public void GetAddition_Input20and50_Returns70()
         {
                 //Arrange
             double number1 = 20;
-            double number2 = 20;
+            double number2 = 50;
             double expectedResult = number1 + number2;
             Calc testCalc = new Calc(number1, number2);
             //Act
